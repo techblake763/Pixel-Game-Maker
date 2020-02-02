0ceb86c6fd5d18215887d8aac7ef53fbea872fef
techblake763 <techblake763@gmail.com>
2020-02-02 09:41:40 +0000

    World 1-1

--- levels.js
+++ levels.js
@@ -1,21 +1,24 @@
-var array = [
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  '                                                                                               ',
-  'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+var array =[
+  '                                                                                                                                                                                       ',
+  '                                                                                                                                                                                       ',
+  '                                                                                                                                                                                       ',
+  '                                                                                                                                                                                       ',
+  '                                                                                                                                                                                       ',
+  '                                                                                                                                                                                       ',
+  '                                                                                                                                                                                       ',
+  '                                                                                                                                                                                @      ',
+  '                                                                                                                                                                        XX      X      ',
+  '          ?                                                                                                                                                            XXX      X      ',
+  '                                                               XXXXXXXXXXX   XXX?              ?            XXX  X??X                                                 XXXX      X      ',
+  '                                                                                                                                                                     XXXXX      X      ',
+  '                                                                                                                          X  X          XX  X                       XXXXXX      X      ',
+  '   ?    X?X?X         XX       XX        XX                 X?X                 ?     X?    ?  ?  ?       X       XX     XX  XX        XXX  XX         XX?X        XXXXXXX      X      ',
+  '               XX     XX       XX        XX                                                                             XXX  XXX      XXXX  XXX     XX         XX XXXXXXXX      X      ',
+  'P        &     XX     XX     & XX &    & XX                                             &&   &     &&          && &&   XXXX  XXXX    XXXXX  XXXX    XX &   &   XXXXXXXXXXX@@@@@@X      ',
+  'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb  bbbbbbbbbbbbbbbbb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbMMbbbbbbbbbbbbb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+  'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb##bbbbbbbbbbbbbbbbb##bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb##bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+  'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+  'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
 ];
 var leveler = [
   array
